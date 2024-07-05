5b813989029d23a25e7a42ef9676dc6eb262d929
Mehrsa Ramezani <Mehrsa.Ramezani@softwire.com>
2024-07-05 13:56:33 +0300

    created my bot

--- src/bots/randomBot.ts
+++ src/bots/randomBot.ts
@@ -1,33 +1,30 @@
 import { Gamestate, BotSelection } from '../models/gamestate';
 
 class Bot {
-    dynamiteCount = 100;
+    dynamiteCount = 0;
     moves = 'RPSDW';
 
     makeMove(gamestate: Gamestate): BotSelection {
         let result = this.moves.charAt(Math.floor(Math.random() * this.moves.length));
-        if (result == 'D') this.dynamiteCount --;
-        if (this.dynamiteCount == 0) this.moves = 'RPSW';
-         switch(result){
-             case 'R': {
-                 return 'R'
-             }
-             case 'P': {
-                 return 'P'
-             }
-             case 'S': {
-                 return 'S'
-             }
-             case 'D': {
-                 return 'D'
-             }
-             case 'W': {
-                 return 'W'
-             }
-             default: {
-                 return 'R'
-             }
-         }
+        if (result == 'D') this.dynamiteCount ++;
+        if (this.dynamiteCount == 100) this.moves = 'RPSW';
+        switch(result){
+            case 'R': {
+                return 'R'
+            }
+            case 'P': {
+                return 'P'
+            }
+            case 'S': {
+                return 'S'
+            }
+            case 'D': {
+                return 'D'
+            }
+            case 'W': {
+                return 'W'
+            }
+        }
     }
 }
 
